cfaf5712b42769ce636ceaba085c1d439efae652
khashayar77 <55273627+khashayar77@users.noreply.github.com>
2019-10-14 08:52:33 +0330

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
