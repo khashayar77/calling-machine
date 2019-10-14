2fd80ccc714f73fb1c8e60ec63e79cf4f83f01f3
MohammadSadegh Mansouri <ma_mansouri@rasana.ir>
2019-10-14 12:00:01 +0330

    .

--- src/app/interceptors/http-error.interceptor.ts
+++ src/app/interceptors/http-error.interceptor.ts
@@ -1,35 +1,36 @@
 import {
-	HttpEvent,
-	HttpInterceptor,
-	HttpHandler,
-	HttpRequest,
-	HttpResponse,
-	HttpErrorResponse
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
 export class HttpErrorInterceptor implements HttpInterceptor {
-	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		return next.handle(request).pipe(
-			retry(1),
-			catchError((error: HttpErrorResponse) => {
-				// tslint:disable-next-line: no-debugger
-				debugger;
-				let errorMessage = '';
-				if (error.error instanceof ErrorEvent) {
-					// client-side error
-					errorMessage = `Error: ${error.error.message}`;
-				} else {
-					// server-side error
-					errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-				}
-				window.alert(errorMessage);
+  snackbar: any;
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        // tslint:disable-next-line: no-debugger
+        debugger;
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        window.alert(errorMessage);
 
-				//this.snackbar.open('اطلاعات اشتباه بود', null, { duration: 999 });
+        this.snackbar.open('اطلاعات اشتباه بود', null, { duration: 999 });
 
-				return throwError(errorMessage);
-			})
-		);
-	}
+        return throwError(errorMessage);
+      })
+    );
+  }
 }
