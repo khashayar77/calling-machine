36e5e51e6a2e747ab04c9d7a507cccd8a6afb57a
SaeedehDeldar <saeedeh.deldar@gmail.com>
2019-10-23 15:41:46 +0330

    merge

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -12,22 +12,22 @@ import { Department } from '../interfaces/department.interface';
 	styleUrls: [ './departments.component.scss' ]
 })
 export class DepartmentsComponent implements OnInit {
-  // tslint:disable-next-line: no-use-before-declare
-  dataSource = new MatTableDataSource<Department>(ELEMENT_DATA);
-  // tslint:disable-next-line: max-line-length
-  displayedColumns: string[] = [
-    'info',
-    'priority',
-    'department',
-    'Unit',
-    'start_working_time',
-    'end_working_time',
-    'queue',
-    'query_method',
-    'call_more',
-    'retry_time',
-    'action'
-  ];
+	// tslint:disable-next-line: no-use-before-declare
+	dataSource = new MatTableDataSource<Department>(ELEMENT_DATA);
+	// tslint:disable-next-line: max-line-length
+	displayedColumns: string[] = [
+		'info',
+		'priority',
+		'department',
+		'Unit',
+		'start_working_time',
+		'end_working_time',
+		'queue',
+		'query_method',
+		'call_more',
+		'retry_time',
+		'action'
+	];
 
 	selectedDepartment: Department;
 
@@ -70,82 +70,6 @@ export class DepartmentsComponent implements OnInit {
 }
 
 const ELEMENT_DATA: Department[] = [
-<<<<<<< HEAD
-  {
-    info: 'jafarian',
-    id: 1,
-    priority: 7,
-    department: 'qm-pcs',
-    Unit: 'cts',
-    start_working_time: '8:00:00',
-    end_working_time: '17:50:00',
-    dialplan_context: 'calling-machine-cmqm1-start',
-    queue: 'cmqm1',
-    query_method: 'QS1',
-    call_more: 1,
-    retry_time: 180
-  },
-  {
-    info: 'jafarian',
-    id: 1,
-    priority: 7,
-    department: 'qm-pcs',
-    Unit: 'qm',
-    start_working_time: '8:00:00',
-    end_working_time: '17:50:00',
-    // nwd_table_id: 1,
-    dialplan_context: 'calling-machine-cmqm1-start',
-    queue: 'cmqm1',
-    query_method: 'QS1',
-    call_more: 1,
-    retry_time: 180
-  },
-  {
-    info: 'jafarian',
-    id: 2,
-    priority: 7,
-    department: 'qm-pcs',
-    Unit: 'pcs',
-    start_working_time: '8:00:00',
-    end_working_time: '17:50:00',
-    // nwd_table_id: 1,
-    dialplan_context: 'calling-machine-cmqm1-start',
-    queue: 'cmqm1',
-    query_method: 'QS1',
-    call_more: 1,
-    retry_time: 180
-  },
-  {
-    info: 'jafarian',
-    id: 3,
-    priority: 7,
-    department: 'qm-pcs',
-    Unit: 'qm',
-    start_working_time: '8:00:00',
-    end_working_time: '17:50:00',
-    // nwd_table_id: 1,
-    dialplan_context: 'calling-machine-cmqm1-start',
-    queue: 'cmqm1',
-    query_method: 'QS1',
-    call_more: 1,
-    retry_time: 180
-  },
-  {
-    info: 'jafarian',
-    id: 4,
-    priority: 7,
-    department: 'qm-pcs',
-    Unit: 'cts',
-    start_working_time: '8:00:00',
-    end_working_time: '17:50:00',
-    // nwd_table_id: 1,
-    dialplan_context: 'calling-machine-cmqm1-start',
-    queue: 'cmqm1',
-    query_method: 'QS1',
-    call_more: 1,
-    retry_time: 180
-  }
-=======
 	{
 		info: 'jafarian',
 		id: 1,
@@ -220,5 +144,4 @@ const ELEMENT_DATA: Department[] = [
 		call_more: 1,
 		retry_time: 180
 	}
->>>>>>> 146e46f8cd143af3b3b7d78303e1d65e3783e53b
 ];
